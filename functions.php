d3db694658eb7b42c381387fbb2381f23756299a
Darcy Christ <darcy@1000camels.com>
2020-04-15 02:57:51 +0000

    checking in theme

--- functions.php
+++ functions.php
@@ -1,13 +1,9 @@
 <?php
 /**
- * Child theme functions
+ * Justice Centre Hong Kong theme functions
  *
- * When using a child theme (see http://codex.wordpress.org/Theme_Development
- * and http://codex.wordpress.org/Child_Themes), you can override certain
- * functions (those wrapped in a function_exists() call) by defining them first
- * in your child theme's functions.php file. The child theme's functions.php
- * file is included before the parent theme's file, so the child theme
- * functions would be used.
+ * Only code that is specific to the theme should go here. Otherwise, place it in the Justice Centre Extensions
+ * plugin
  *
  * Text Domain: oceanwp
  * @link http://codex.wordpress.org/Plugin_API
@@ -33,17 +29,52 @@ add_action( 'wp_enqueue_scripts', 'oceanwp_child_enqueue_parent_style' );
 /** 
  * Replace the default color palettes in the color picker
  */
-function my_custom_color_palettes( $palettes ) {
-    $palettes = array(
-        '#34ade2',
-        '#625528',
-        '#b28238',
-        '#2a282b',
-        '#000000',
-        '#ffffff',
-        '#cccccc',
-    );
+// function my_custom_color_palettes( $palettes ) {
+//     $palettes = array(
+//         '#920000',
+//         '#760000',
+//         '#670001',
+//         '#3e3e3e',
+//         '#e8e8e8',
+//         '#000000',
+//         '#ffffff',
+//     );
 
-    return $palettes;
+//     return $palettes;
+// }
+// add_filter( 'ocean_default_color_palettes', 'my_custom_color_palettes' );
+
+
+/**
+ * Alter your post layouts
+ *
+ * Replace is_singular( 'post' ) by the function where you want to alter the layout
+ * @return full-width, full-screen, left-sidebar or right-sidebar
+ *
+ */
+function custom_post_layout_class( $class ) {
+
+    // Alter your layout
+    if ( is_singular() ) {
+        $class = 'full-width';
+    }
+
+    // Return correct class
+    return $class;
+
 }
-add_filter( 'ocean_default_color_palettes', 'my_custom_color_palettes' );
+add_filter( 'ocean_post_layout_class', 'custom_post_layout_class', 20 );
+
+
+/**
+ * Suppress errors
+ */
+add_filter('deprecated_constructor_trigger_error', '__return_false');
+
+/**
+ * Load only our designated fonts
+ */
+function myprefix_google_fonts( $array ) {
+    return array( 'Open Sans', 'Open Sans Condensed', 'News Cycle' );
+}
+//add_filter( 'ocean_google_fonts_array', 'myprefix_google_fonts' );
